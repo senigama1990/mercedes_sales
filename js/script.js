6f4fd1895c3c663a36d80c10d7d19430b50e8aa3
senigama1990 <senigama1990@mail.ru>
2021-11-30 13:30:33 +0500

    'tabs'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
+    //LOADER
     setTimeout(function () {
         const loader = document.querySelector('.loader')
         loader.style.opacity = '0'
@@ -6,4 +7,35 @@ window.addEventListener('DOMContentLoaded', function () {
             loader.style.display = 'none'
         }, 500)
     }, 1000)
+
+    //TABS
+    const tabs = document.querySelectorAll('.tabheader__item')
+    const tabContent = document.querySelectorAll('.tabcontent')
+    const headerParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabContent.forEach(item => {
+            item.style.display = 'none'
+        })
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active')
+        })
+    }
+    function showTabContent(i = 0) {
+        tabContent[i].style.display = 'block'
+        tabs[i].classList.add('tabheader__item_active')
+    }
+    hideTabContent()
+    showTabContent()
+
+    headerParent.addEventListener('click', function (e) {
+        if (e.target && e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (e.target == item) {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            })
+        }
+    })
 })
